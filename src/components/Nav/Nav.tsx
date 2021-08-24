0059b7374ab00c548d2b914bdd81e4efff346db4
Vonta Johnson <devontajohnsonit@gmail.com>
2021-08-24 15:28:51 -0400

    initial commit for crunchyroll project

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import './Nav.css'
+
+export default function Nav() {
+  return (
+    <header role="navigation" aria-label="Navbar" className='App-header'>
+      <ul aria-label="Navigation List">
+          <li>Home</li>
+          <li>About</li>
+      </ul>
+    </header>
+  )
+}
