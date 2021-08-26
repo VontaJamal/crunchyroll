2eae1bf6f02eef03fa6013624ea00190b4e22ae1
Vonta Johnson <devontajohnsonit@gmail.com>
2021-08-26 15:54:53 -0400

    started the nav for the project

--- src/components/Nav/Nav.test.tsx
+++ src/components/Nav/Nav.test.tsx
@@ -1,9 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import App from "../../App";
 import Nav from "./Nav";
 
 it('should render the nav component', () => {
   render(<Nav/>)
-  const navList = screen.getByRole('navigation')
-  expect(navList).toBeInTheDocument()
+  const navHeader = screen.getByRole('navigation')
+  expect(navHeader).toBeInTheDocument()
+});
+
+it('should render a ul with a role of Navigation List', () => {
+  render(<Nav/>)
+  const navList = screen.getByLabelText('Navigation List')
+  expect(navList).toBeInTheDocument
+})
+
+it('Should have 4 children in the Navigation List', () => {
+  render(<Nav/>)
+  const navListItems = screen.queryAllByRole('listitem');
+  expect(navListItems).toHaveLength(4)
 })
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,10 +2,12 @@ import './Nav.css'
 
 export default function Nav() {
   return (
-    <header role="navigation" aria-label="Navbar" className='App-header'>
-      <ul aria-label="Navigation List">
-          <li>Home</li>
-          <li>About</li>
+    <header role='navigation' aria-label='Navbar' className='nav'>
+      <ul aria-label='Navigation List'>
+        <li>Updated</li>
+        <li>Popular</li>
+        <li>Simulcasts</li>
+        <li>All</li>
       </ul>
     </header>
   )
