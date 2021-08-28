34e9e126cfe21350ed51a031e8d60e87622fd1a2
Vonta Johnson <devontajohnsonit@gmail.com>
2021-08-27 21:50:54 -0400

    finish adding nav elements

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,14 @@
 import './Nav.css'
+import logo from '../../images/logo.png'
 
 export default function Nav() {
   return (
     <header role='navigation' aria-label='Navbar' className='nav'>
-
+      <div className="logo_container">
+        <img src={logo} alt='The Crunchyroll logo'/>
+        <p>crunchyroll</p>
+      </div>
+      <h2 className="animeText">Anime</h2>
       <ul aria-label='Navigation List'>
         <li>Updated</li>
         <li>Popular</li>
